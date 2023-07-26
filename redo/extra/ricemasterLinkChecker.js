161cd940fc8412c09a3e7a4e363b999106d01e94
Oscaperion <brandondancer@protonmail.com>
2023-07-26 21:15:39 +0300

    Ricemaster script finished for now

--- redo/extra/ricemasterLinkChecker.js
+++ redo/extra/ricemasterLinkChecker.js
@@ -15,8 +15,8 @@ let checkDates = [201003,202006];
 // ===============================
 
 let checkedUid = -1;
-let reups = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/reuploads.json', 'utf8'));
-reups = reups.map(item => item.id);
+let reupsAll = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/reuploads-v2.json', 'utf8'));
+reups = reupsAll.map(item => item.id);
 
 {
    let userList = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/youtubeUserList2.json', 'utf8'));
@@ -41,7 +41,7 @@ if (checkDates[0] < checkDates[1]) {
 }
 
 const br =  '\r\n';
-let gatheredLinks = [];
+//let gatheredLinks = [];
 
 for (let k = checkDates[0]; k >= checkDates[1]; k--) {
    try {
@@ -53,6 +53,8 @@ for (let k = checkDates[0]; k >= checkDates[1]; k--) {
    }
 }
 
+fs.writeFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/reuploads.json', JSON.stringify(reupsAll));
+
 function checkFile(checkedFile) {
    for (let p = 0; p < checkedFile.length; p++) {
       if (checkedUid === checkedFile[p].uId && !reups.includes(checkedFile[p].id)) checkVideo(checkedFile[p]);
@@ -61,7 +63,7 @@ function checkFile(checkedFile) {
 
 function checkVideo(checkedVideo) {
    console.log(checkedVideo.id);
-   
+
    let desc = checkedVideo.description;
    
    let findStr1 = 'watch/';
@@ -72,21 +74,22 @@ function checkVideo(checkedVideo) {
    if (tmpStr1 !== -1) {
       //console.log("Lollero");
 
-   // console.log(tmpStr1);
+      // console.log(tmpStr1);
 
-   let tmpStr2 = desc.substring(tmpStr1 + findStr1.length);
+      let tmpStr2 = desc.substring(tmpStr1 + findStr1.length);
 
-   let tmpStr3 = tmpStr2.indexOf(findStr2);
-   let tmpStr4;
-   if (tmpStr3 > -1) tmpStr4 = tmpStr2.substring(0,tmpStr3).trim();
-   else tmpStr4 = tmpStr2.trim();
+      let tmpStr3 = tmpStr2.indexOf(findStr2);
+      let tmpStr4 = tmpStr2.trim();
+      if (tmpStr3 > -1) tmpStr4 = tmpStr2.substring(0,tmpStr3).trim();
+      //else tmpStr4 = tmpStr2.trim();
 
-   let retTmp = {};
+      let retTmp = {};
 
-   retTmp["id"] = checkedVideo.id;
-   retTmp["reup"] = tmpStr4;
-   retTmp["reup_site"] = "Niconico";
+      retTmp["reup_site"] = "Niconico";
+      retTmp["id"] = checkedVideo.id;
+      retTmp["reup"] = tmpStr4;
 
-   console.log(retTmp);
+      reupsAll.push(retTmp);
+      console.log(retTmp);
    }
 }
