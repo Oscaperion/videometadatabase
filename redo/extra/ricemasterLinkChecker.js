84cb6f97f8720df2d86b3fdb3657cc1f17e4724b
Oscaperion <brandondancer@protonmail.com>
2023-07-26 19:14:42 +0300

    Another update on the Ricemaster script

--- redo/extra/ricemasterLinkChecker.js
+++ redo/extra/ricemasterLinkChecker.js
@@ -4,15 +4,19 @@ var fs = require('fs');
 const url = require('url');
 const http = require('http');
 
+// ===============================
 // Easy changes can be made here:
 
 let searchingUser = "Rlcemaster3";
-// Make sure these are in form "YYYYMMDD"
-let checkDates = ["20100300","20200600"];
 
-// =============================
+// Make sure the dates are in form "YYYYMMDD"
+let checkDates = [201003,202006];
+
+// ===============================
 
 let checkedUid = -1;
+let reups = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/reuploads.json', 'utf8'));
+reups = reups.map(item => item.id);
 
 {
    let userList = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/youtubeUserList2.json', 'utf8'));
@@ -37,9 +41,24 @@ if (checkDates[0] < checkDates[1]) {
 }
 
 const br =  '\r\n';
-//var videoList = '';
+let gatheredLinks = [];
 
-var gatheredIds = [];
-//var gatheredJson = [];
+for (let k = checkDates[0]; k >= checkDates[1]; k--) {
+   try {
+      let checkingFile = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/split_parts2/vids' + k + '.json', 'utf8'));
+      console.log(k);
+      checkFile(checkingFile);
+   } catch (error) {
+      continue;
+   }
+}
 
-console.log("Tallennettu");
+function checkFile(checkedFile) {
+   for (let p = 0; p < checkedFile.length; p++) {
+      if (checkedUid === checkedFile[p].uId && !reups.includes(checkedFile[p].id)) checkVideo(checkedFile[p]);
+   }
+}
+
+function checkVideo(checkedVideo) {
+   console.log(checkedVideo.id);
+}
