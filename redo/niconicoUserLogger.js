3f057ebd970510595c99eb2829364d195e51104a
Oskari Oksanen <oskari14@hotmail.com>
2024-03-16 19:05:33 +0200

    Tweaked the scripts and added so this missing user info is processed during the stack

--- redo/niconicoUserLogger.js
+++ redo/niconicoUserLogger.js
@@ -5,6 +5,7 @@ let fs = require('fs');
 let userIds = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/niconicoUserList.json', 'utf8'));
 
 let vids = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/split_parts/vids53.json', 'utf8'));
+let missingUploaders = JSON.parse(fs.readFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/missingNicoUid2.json', 'utf8'));
 
 for (let orttt = 0; orttt < vids.videos.length; orttt++) {
    if (vids.videos[orttt].extractor_key === "Niconico") {
@@ -12,6 +13,14 @@ for (let orttt = 0; orttt < vids.videos.length; orttt++) {
          userIds.push(vids.videos[orttt].uploader_id);
          console.log("Added " + vids.videos[orttt].uploader_id);
       }
+   }
+}
+
+for (let k = 0; k < missingUploaders.length; k++) {
+   let tmpEntry = missingUploaders[k];
+   if (tmpEntry.nicologEntry && !userIds.includes(tmpEntry.uId)) {
+      userIds.push(tmpEntry.uId);
+      console.log("Added " + tmpEntry.uId);
    }
 }
 
