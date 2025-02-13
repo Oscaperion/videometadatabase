d6780544603e7731be8919857ad77899b09670dc
Oskari Oksanen <oskari14@hotmail.com>
2025-02-13 16:24:28 +0200

    Further optimizing, now done with jsonDownloadLister

--- redo/jsonDownloadLister.js
+++ redo/jsonDownloadLister.js
@@ -5,34 +5,39 @@ const url = require('url');
 const http = require('http');
 
 const br =  '\r\n';
-//var videoList = '';
 
+const idSet = new Set();
+// const extractorSet = new Set();
 
-//var exampleFile = fs.readFileSync('massJsonTesting\\bagiIEpLiLI.info.json', 'utf8');
-//var parsedJSON = JSON.parse(exampleFile);
-
-//console.log(exampleFile);
-
-//var toBeSortedList = [];
-    /*
-videoList += '<hr/>' + br;
-        videoList += '<div><b>' + parsedJSON.title + '</b><br/>' + br;
-        videoList += '<code><a href=\"' + parsedJSON.webpage_url + '\" target=\"_blank\">' + parsedJSON.webpage_url + '</a></code><br/><br/>' + br;
-        videoList += 'Uploader: <a href=\"' + parsedJSON.uploader_url + '\" target=\"_blank\">' + parsedJSON.uploader + ' (<code>' + parsedJSON.uploader_id + '</code>)</a><br/>' + br;
-        videoList += 'Release date: ' + parsedJSON.upload_date + '</div>' + br;       */
-//console.log('Started forming the server')  ;
-
-var rString = '';
-var tu;
-for (tu = 63; tu >= 0; tu--) {
+//var rString = '';
+//let tu;
+for (let tu = 63; tu >= -1; tu--) {
 //var videoitaFile = fs.readFileSync('videoita.json', 'utf8');
 //var parsedVideos = JSON.parse(videoitaFile);
-    var parsedVideos = JSON.parse(fs.readFileSync(('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/split_parts/vids' + tu + '.json'), 'utf8'));
+    let filepath = 'F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/split_parts/vids' + tu + '.json';
+    if (tu === -1) filepath = 'F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/split_parts/finnredo.json';
+    let parsedVideos = JSON.parse(fs.readFileSync(filepath, 'utf8')).videos;
+    console.log("Checking " + filepath);
 
 //var k = 25;
 //var searchWord = 'thwy';
 
+    let parsedIds = parsedVideos.filter(ent => { return !(ent.extractor_key === "BilibiliSpaceVideo" || 
+                                                            ent.extractor_key === "YoutubeTab" ||
+                                                            ent.extractor_key === "SoundcloudPlaylist"); })
+                                .map(ent => {
+                                   // extractorSet.add(ent.extractor_key);
+                                   let idTmp = ent.id;
+                                   if (Array.isArray(idTmp)) idTmp = ent.id[0];
+                                   return ent.extractor_key.toLowerCase() + ' ' + idTmp;});
 
+    // console.log(parsedIds);
+
+    // idSet.add(...parsedIds);
+    
+    parsedIds.forEach(ent => { idSet.add(ent);  });
+
+              /*
     //for (i = 0; i < 100; i++) {
     for (i = 0; i < parsedVideos.videos.length; i++) {
        //var dewIt = true;
@@ -40,7 +45,7 @@ for (tu = 63; tu >= 0; tu--) {
        if (parsedVideos.videos[i].extractor_key === "BilibiliSpaceVideo") continue; // dewIt = false;
 
        if (parsedVideos.videos[i].extractor_key === "YoutubeTab") continue; // dewIt = false;
-       
+
        let tmpId = parsedVideos.videos[i].id;
 
        if (parsedVideos.videos[i].extractor_key === "BiliBili" && Array.isArray(parsedVideos.videos[i].id)) tmpId = parsedVideos.videos[i].id[0];
@@ -50,11 +55,12 @@ for (tu = 63; tu >= 0; tu--) {
        let vidoId = parsedVideos.videos[i].extractor_key.toLowerCase() + ' ' + tmpId;
 
        rString += vidoId + '\n';
-    }
+    }       */
 }
 
-    fs.writeFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/already-downloaded.txt', rString);
+    fs.writeFileSync('F:/Dropbox/NodeJS/YTPMV Metadata Archive JSON/already-downloaded.txt', Array.from(idSet).join('\n') + '\n' );
     console.log("Tallennettu");
+    // console.log(extractorSet);
 
 //console.log('List done');
 
